30f91e5e956e6ba2ac3dc452a2c589bc3057f727
itsmarsss <itsmarzzzzzz@gmail.com>
2024-04-12 22:11:18 -0400

    Add all bot slash commands

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const { Client, REST, Routes, GatewayIntentBits } = require("discord.js");
+const { Client, REST, Routes, GatewayIntentBits, ApplicationCommandOptionType } = require("discord.js");
+const { mongoClient } = require("./mongodb");
+
 require("dotenv").config();
+
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID;
 
@@ -8,6 +11,96 @@ const run = async () => {
         {
             name: 'ping',
             description: 'Replies with Pong!',
+        },
+        {
+            name: 'team',
+            description: 'Team commands',
+            options: [
+                {
+                    name: 'create',
+                    description: 'Create a team',
+                    type: ApplicationCommandOptionType.Subcommand,
+                    options: [
+                        {
+                            name: 'name',
+                            description: 'Team name',
+                            type: ApplicationCommandOptionType.String,
+                            required: true,
+                        }
+                    ]
+                },
+                {
+                    name: 'invite',
+                    description: 'Invite a user to the team',
+                    type: ApplicationCommandOptionType.Subcommand,
+                    options: [
+                        {
+                            name: 'user',
+                            description: 'Invite user',
+                            type: ApplicationCommandOptionType.User,
+                            required: true,
+                        }
+                    ]
+                },
+                {
+                    name: 'accept',
+                    description: 'Accept an invitation to join a team',
+                    type: ApplicationCommandOptionType.Subcommand,
+                    options: [
+                        {
+                            name: 'team',
+                            description: 'Accept invite',
+                            type: ApplicationCommandOptionType.String,
+                            required: true,
+                        }
+                    ]
+                },
+                {
+                    name: 'kick',
+                    description: 'Kick a user from the team',
+                    type: ApplicationCommandOptionType.Subcommand,
+                    options: [
+                        {
+                            name: 'user',
+                            description: 'Kick user',
+                            type: ApplicationCommandOptionType.String,
+                            required: true,
+                        }
+                    ]
+                },
+            ],
+        },
+        {
+            name: 'profile',
+            description: 'Profile commands',
+            options: [
+                {
+                    name: 'about',
+                    description: 'Set or view your profile information',
+                    type: ApplicationCommandOptionType.Subcommand,
+                    options: [
+                        {
+                            name: 'about_me',
+                            description: 'About me',
+                            type: ApplicationCommandOptionType.String,
+                            required: true,
+                        }
+                    ]
+                },
+                {
+                    name: 'view',
+                    description: 'View a user\'s profile',
+                    type: ApplicationCommandOptionType.Subcommand,
+                    options: [
+                        {
+                            name: 'user',
+                            description: 'View user',
+                            type: ApplicationCommandOptionType.User,
+                            required: true,
+                        }
+                    ]
+                },
+            ],
         },
     ];
 
@@ -32,13 +125,20 @@ const run = async () => {
     client.on('interactionCreate', async interaction => {
         console.log(interaction.user.username);
 
-        if (!interaction.isChatInputCommand()) return;
+        if (!interaction.isCommand()) return;
 
         if (interaction.commandName === 'ping') {
-            await interaction.reply('Pong!');
+            await interaction.reply('Pong! <@' + interaction.user.id + ">");
+        }
+
+        if (interaction.commandName === 'team') {
+            await interaction.reply('Team! <@' + interaction.user.id + ">");
+        }
+
+        if (interaction.commandName === 'profile') {
+            await interaction.reply('Profile! <@' + interaction.user.id + ">");
         }
     });
-
 
     client.on('guildMemberAdd', member => {
         console.log(member.user.username);
