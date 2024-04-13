ab3fdff6aa340a7adfd86d1f89c45be35aebe388
itsmarsss <itsmarzzzzzz@gmail.com>
2024-04-12 22:11:02 -0400

    Add MongoDB connector

---
+++ mongodb.js
@@ -0,0 +1,24 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+require("dotenv").config();
+
+let mongoClient = null;
+
+const mongoURI = process.env.MONGO_URI;
+console.log(mongoURI);
+
+const initialize = async () => {
+    mongoClient = new MongoClient(mongoURI, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        },
+    });
+    await mongoClient.connect();
+    console.log("Connected to MongoDB");
+};
+
+initialize();
+
+module.exports = { mongoClient, initialize };
